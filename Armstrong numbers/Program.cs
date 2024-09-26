56eaefce7b9ad1ef9a894423f94f36fae9a50398
карина <@DESKTOP-FP9I0KA>
2024-09-26 14:53:37 +0300

    Add project files.

---
+++ Armstrong numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Armstrong_numbers
+{
+    internal class Program
+    {
+        public static bool IsArmstrongNumber(int number)
+        {
+            
+            string numberStr = number.ToString();
+            int numberOfDigits = numberStr.Length;
+
+            int sum = 0;
+
+           
+            foreach (char digitChar in numberStr)
+            {
+               
+                int digit = (int)char.GetNumericValue(digitChar);
+
+               
+                sum += (int)Math.Pow(digit, numberOfDigits);
+            }
+
+            
+            return sum == number;
+        }
+
+        static void Main()
+        {
+            
+            int[] numbersToCheck = { 9, 10, 153, 154 };
+
+            foreach (int number in numbersToCheck)
+            {
+                bool result = IsArmstrongNumber(number);
+                Console.WriteLine($"{number} є числом Армстронга: {result}");
+            }
+        }
+    }
+}
